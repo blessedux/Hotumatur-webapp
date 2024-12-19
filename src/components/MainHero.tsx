376247f97f8e75d51c2a282c4e1833c4d95df29c
Joaquin <jfarfantorres@gmail.com>
2024-12-19 09:22:05 -0300

    added custom font

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -1,4 +1,8 @@
 import TourSelector from '@/components/TourSelector'
+import { Satisfy } from '@next/font/google';
+
+const satisfyFont = Satisfy({ subsets: ['latin'], weight: '400' });
+
 
 export default function HeroSection() {
     return (
@@ -19,7 +23,7 @@ export default function HeroSection() {
             <div className="absolute inset-0 bg-black/40" />
             <div className="relative z-10 flex h-full flex-col items-center justify-center px-4">
                 <p className="mb-4 text-center text-md md:text-2xl text-white">
-                    Ven a conocer la magia de Rapa Nui
+                    Ven a conocer la magia de <span className={`${satisfyFont.className}`}>Rapa Nui</span>
                 </p>
 
                 <h1 className="mb-10 md:mb-16 text-center text-3xl font-bold text-white md:text-6xl">
