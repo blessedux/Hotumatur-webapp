5135a1ba0daea26850c761076fbf07592015d9a2
Bzzmn <alvaro.acevedo.ing@gmail.com>
2024-12-19 18:16:50 -0300

    feature/add rentals on navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -82,6 +82,10 @@ export default function NavBar() {
                             </MenuItems>
                         </Transition>
                     </Menu>
+
+                    <Link href="#" className="text-white hover:text-white/80">
+                        Rentals
+                    </Link>
                     <Link href="/nosotros" className="text-white hover:text-white/80">
                         Nosotros
                     </Link>
