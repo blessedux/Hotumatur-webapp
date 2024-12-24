ae69b42707825a7133538f1b4fb08da6d467aa87
Joaquin <jfarfantorres@gmail.com>
2024-12-24 06:52:58 -0300

    minimized security in middleware and added cloudflare as trusted csp

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,22 @@
 import { NextResponse } from 'next/server'
 
-export function middleware() {
+export function middleware(req: Request) {
     const response = NextResponse.next()
 
+    // Add Content-Security-Policy header
     response.headers.set(
         'Content-Security-Policy',
-        `
-            default-src 'self';
-            script-src 'self' 'unsafe-inline' 'unsafe-eval';
-            style-src 'self' 'unsafe-inline';
-            img-src 'self' data: https://backend.hotumatur.com https://vumbnail.com;
-            font-src 'self';
-            connect-src 'self';
-            frame-src 'self' https://player.vimeo.com;
-        `.replace(/\s+/g, ' ').trim()
+        [
+            "default-src 'self';",
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.cloudflareinsights.com;",
+            "style-src 'self' 'unsafe-inline';",
+            "img-src 'self' data: https://backend.hotumatur.com https://vumbnail.com;",
+            "font-src 'self';",
+            "connect-src 'self';",
+            "frame-src 'self' https://player.vimeo.com;"
+        ]
+            .join(' ')
+            .trim()
     )
 
     return response
