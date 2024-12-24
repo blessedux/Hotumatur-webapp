2e9fab95fd6bc84f7aca168d8530ef4e93068875
Joaquin <jfarfantorres@gmail.com>
2024-12-24 06:58:57 -0300

    updated api configuration url endpoint

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { config } from '@/config';
 
 const api = axios.create({
-    baseURL: config.woocommerce.url,
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://backend.hotumatur.com', // Ensure this URL is correct
     auth: {
         username: config.woocommerce.consumerKey!,
         password: config.woocommerce.consumerSecret!
