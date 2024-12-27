72de17d233786c9983a12432a7d84bb553036a20
Joaquin <jfarfantorres@gmail.com>
2024-12-27 09:56:59 -0300

    updated layout.tsx opengraph data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,9 @@ export const metadata: Metadata = {
     siteName: 'Hotumatur Rapa Nui',
     images: [
       {
-        url: '/images/Hotumatur-android-chrome-512x512.png',
-        width: 800,
-        height: 600,
+        url: 'https://backend.hotumatur.com/wp-content/uploads/2024/12/Hotumatur-android-chrome-512x512-1.png',
+        width: 512,
+        height: 512,
         alt: 'Hotumatur: Agencia de turismo en Rapa Nui',
       },
     ],
@@ -38,7 +38,8 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: 'Hotumatur: agencia de turismo',
     description: 'Vive la magia de Isla de Pascua con Hotumatur, los embajadores oficiales de Rapa Nui. Ofrecemos experiencias culturales auténticas, alquiler de vehículos y aventuras inolvidables que te conectan con el corazón de esta isla legendaria. Descubre la historia, la cultura y la impresionante belleza de Rapa Nui junto a nuestros expertos guías locales',
-    images: ['/images/Hotumatur-android-chrome-512x512.png'],
+    url: 'https://backend.hotumatur.com/wp-content/uploads/2024/12/Hotumatur-android-chrome-512x512-1.png',
+    imageAlt: 'Hotumatur: Agencia de turismo en Rapa Nui',
   },
 };
 
@@ -50,6 +51,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta property="og:description" content={metadata.openGraph.description} />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:site_name" content={metadata.openGraph.siteName} />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        <meta property="og:image:alt" content={metadata.openGraph.images[0].alt} />
+        <meta property="og:image:width" content={metadata.openGraph.images[0].width.toString()} />
+        <meta property="og:image:height" content={metadata.openGraph.images[0].height.toString()} />
+        <meta property="og:locale" content="es_CL" />
+        <meta property="og:locale:alternate" content="en_US" />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta name="twitter:card" content={metadata.twitter.card} />
+        <meta name="twitter:title" content={metadata.twitter.title} />
+        <meta name="twitter:description" content={metadata.twitter.description} />
+        <meta name="twitter:image" content={metadata.twitter.images[0]} />
+        <meta name="twitter:image:alt" content={metadata.twitter.imageAlt} />
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}>
