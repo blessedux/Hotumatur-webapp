5a3fb38de5ef51e22f88fb71b86d9d0380b1a3a5
Joaquin <jfarfantorres@gmail.com>
2024-12-27 10:00:20 -0300

    fixed build issue updated opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
         <meta property="og:description" content={metadata.openGraph.description} />
         <meta property="og:url" content={metadata.openGraph.url} />
         <meta property="og:site_name" content={metadata.openGraph.siteName} />
-        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        <meta property="og:image" content={metadata.openGraph.images?.[0]?.url || '/default-image.png'} />
         <meta property="og:image:alt" content={metadata.openGraph.images[0].alt} />
         <meta property="og:image:width" content={metadata.openGraph.images[0].width.toString()} />
         <meta property="og:image:height" content={metadata.openGraph.images[0].height.toString()} />
@@ -67,7 +67,7 @@ export default function RootLayout({
         <meta name="twitter:card" content={metadata.twitter.card} />
         <meta name="twitter:title" content={metadata.twitter.title} />
         <meta name="twitter:description" content={metadata.twitter.description} />
-        <meta name="twitter:image" content={metadata.twitter.images[0]} />
+        <meta name="twitter:image" content={metadata.twitter?.images?.[0] || '/default-image.png'} />
         <meta name="twitter:image:alt" content={metadata.twitter.imageAlt} />
         <link rel="icon" href="/favicon.ico" />
       </head>
